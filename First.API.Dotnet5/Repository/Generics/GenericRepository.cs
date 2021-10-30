03b3e98b422347343f604eee18b370ece31eb0da
Dharuan Emina <dharuan.emina.tc@take.net>
2021-10-30 19:29:09 -0300

    Implements generic repositories

---
+++ First.API.Dotnet5/Repository/Generics/GenericRepository.cs
@@ -0,0 +1,96 @@
+﻿using First.API.Dotnet5.Model.Base;
+using First.API.Model.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First.API.Dotnet5.Repository.Generics
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly MySQLContext _context;
+
+        private DbSet<T> _dataSet;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            _dataSet = context.Set<T>();
+        }
+
+        public T Create(T item)
+        {
+            try
+            {
+                _dataSet.Add(item);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return item;
+        }
+
+        public void Delete(long id)
+        {
+            try
+            {
+                var result = FindById(id);
+
+                if (result != null)
+                {
+                    _dataSet.Remove(result);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<T> FindAll()
+        {
+            return _dataSet.ToList();
+        }
+
+        public T FindById(long id)
+        {
+            return _dataSet.SingleOrDefault(item => item.Id == id);
+        }
+
+        public T Update(T item)
+        {
+            if (!Exists(item.Id))
+            {
+                return default;
+            }
+
+            var result = FindById(item.Id);
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+            return item;
+        }
+
+        public bool Exists(long id)
+        {
+            return _dataSet.Any(p => p.Id == id);
+        }
+    }
+}
---
+++ First.API.Dotnet5/Repository/Generics/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using First.API.Dotnet5.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First.API.Dotnet5.Repository.Generics
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T Update(T item);
+        void Delete(long id);
+        T FindById(long id);
+        List<T> FindAll();
+        bool Exists(long id);
+    }
+}
