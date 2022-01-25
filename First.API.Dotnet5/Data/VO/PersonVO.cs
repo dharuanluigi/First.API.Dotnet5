e924162a36fd3909a113ed8c04200f7c284316b2
Dharuan Emina <dharuan.emina.tc@take.net>
2022-01-24 22:35:23 -0300

    Implements some custom fields

--- First.API.Dotnet5/Data/VO/PersonVO.cs
+++ First.API.Dotnet5/Data/VO/PersonVO.cs
@@ -1,15 +1,22 @@
-﻿namespace First.API.Dotnet5.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace First.API.Dotnet5.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public int Id { get; set; }
 
+        [JsonPropertyName("Name")]
         public string FirstName { get; set; }
 
+        [JsonPropertyName("Last_Name")]
         public string LastName { get; set; }
 
+        [JsonIgnore]
         public string Address { get; set; }
 
+        [JsonPropertyName("Gen")]
         public string Gender { get; set; }
     }
 }
